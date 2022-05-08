76173ac765b91a66d55d9ac99e2cb80a3482bbe5
Sean <seanlengws@gmail.com>
2022-05-08 17:22:57 +0800

    Added API call to fetch upcoming movies

--- app/src/main/java/com/example/movielist/MainActivity.kt
+++ app/src/main/java/com/example/movielist/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.movielist
 
-import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
 import android.os.Bundle
+import java.io.IOException
+import com.google.gson.GsonBuilder
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    private val apiKey = "3b547d87b9704c60b0e0c03ae26e5cd4"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchUpcomingMovies()
+    }
+
+    private fun fetchUpcomingMovies() {
+        val url = "https://api.themoviedb.org/3/movie/upcoming?api_key=$apiKey"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+//                val json = JSONObject(body)
+                val itemType = object : TypeToken<UpcomingMovies>() {}.type
+                val gson = GsonBuilder().create().fromJson<UpcomingMovies>(body, itemType)
+
+                runOnUiThread {
+                    // This is where your recyclerView adapter should be
+                    // Example: recyclerView.adapter = MainAdapter(gson.results)
+                    // gson.results return an array of Upcoming Movies
+                    // View here to see how the API response looks like https://api.themoviedb.org/3/movie/upcoming?api_key=3b547d87b9704c60b0e0c03ae26e5cd4
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/movielist/UpcomingMovies.kt
@@ -0,0 +1,28 @@
+package com.example.movielist
+
+class UpcomingMovies(
+    val dates: dates,
+    val page: Int,
+    val results: List<Movie>,
+    val total_pages: Int,
+    val total_results: Int
+)
+
+class dates(val maximum: String, val minimum: String)
+
+class Movie (
+    val adult: Boolean,
+    val backdrop_path: String,
+    var genre_ids: List<Int>,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: String,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Float,
+    val vote_count: Int
+)
