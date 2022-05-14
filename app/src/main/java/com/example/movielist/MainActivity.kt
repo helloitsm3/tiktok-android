50d4fbc324ff6a7c0dab2c364e4dc057648350f4
whathellahor <zihaoe1998@gmail.com>
2022-05-14 21:55:02 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/movielist/MainActivity.kt
+++ app/src/main/java/com/example/movielist/MainActivity.kt
@@ -68,84 +68,13 @@ class MainActivity : AppCompatActivity() {
         Log.i("fileList", context.fileList().count().toString() );
     }
 
-//    private fun MovieTypes.fetchMovies() {
-//        val type = when(this) {
-//            MovieTypes.LATEST -> "latest"
-//            MovieTypes.UPCOMING -> "upcoming"
-//            MovieTypes.NOW_PLAYING -> "now_playing"
-//            MovieTypes.TOP_RATED -> "top_rated"
-//            MovieTypes.POPULAR -> "popular"
-//        }
-//
-//        val url = "https://api.themoviedb.org/3/movie/$type?api_key=$apiKey"
-//        val request = Request.Builder().url(url).build()
-//        val client = OkHttpClient()
-//
-//        if (type == "upcoming" || type == "now_playing") {
-//            client.newCall(request).enqueue(object : Callback {
-//                fun onResponse(call: Call, response: Response) {
-//                    val body = response.body?.string()
-//                    val itemType = object : TypeToken<MovieResponse2>() {}.type
-//                    val gson = GsonBuilder().create().fromJson<MovieResponse2>(body, itemType)
-//
-//                    runOnUiThread {
-//                        // This is where your recyclerView adapter should be
-//                        // Example: recyclerView.adapter = MainAdapter(gson.results)
-//                        // gson.results return an array of Upcoming Movies
-//                        // View here to see how the API response looks like https://api.themoviedb.org/3/movie/upcoming?api_key=3b547d87b9704c60b0e0c03ae26e5cd4
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call, e: IOException) {
-//                }
-//            })
-//        } else if (type == "top_rated" || type == "popular") {
-//            client.newCall(request).enqueue(object : Callback {
-//                fun onResponse(call: Call, response: Response) {
-//                    val body = response.body?.string()
-//                    val itemType = object : TypeToken<TopRatedMovies>() {}.type
-//                    val gson = GsonBuilder().create().fromJson<TopRatedMovies>(body, itemType)
-//
-//                    runOnUiThread {
-//                        // This is where your recyclerView adapter should be
-//                        // Example: recyclerView.adapter = MainAdapter(gson.results)
-//                        // gson.results return an array of Upcoming Movies
-//                        // View here to see how the API response looks like https://api.themoviedb.org/3/movie/upcoming?api_key=3b547d87b9704c60b0e0c03ae26e5cd4
-//                    }
-//                }
-//
-//                fun onFailure(call: Call, e: IOException) {
-//                }
-//            })
-//        } else if (type == "latest") {
-//            client.newCall(request).enqueue(object : Callback {
-//                fun onResponse(call: Call, response: Response) {
-//                    val body = response.body?.string()
-//                    val itemType = object : TypeToken<LatestMovies>() {}.type
-//                    val gson = GsonBuilder().create().fromJson<LatestMovies>(body, itemType)
-//
-//                    runOnUiThread {
-//                        // This is where your recyclerView adapter should be
-//                        // Example: recyclerView.adapter = MainAdapter(gson.results)
-//                        // gson.results return an array of Upcoming Movies
-//                        // View here to see how the API response looks like https://api.themoviedb.org/3/movie/upcoming?api_key=3b547d87b9704c60b0e0c03ae26e5cd4
-//                    }
-//                }
-//
-//                fun onFailure(call: Call, e: IOException) {
-//                }
-//            })
-//        }
-//    }
-}
-
 private fun fetchMovieData(callback: (List<PopularMovies>) -> Unit) {
     val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
     apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
-        override fun onFailure(call: Call<MovieResponse>, t: Throwable) { }
+        override fun onFailure(call: Call<MovieResponse>, t: Throwable) {}
 
         override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
             return callback(response.body()!!.popularMovies)
         }
     })
-}
+}}
